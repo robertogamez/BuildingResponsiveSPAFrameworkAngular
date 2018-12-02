c2bf6adbf3093d8fee9dfb49c788011a4da149f1
robertogamez <roberto.mb.gamez@hotmail.com>
2018-12-02 11:45:06 -0600

    Se crea contenido de componente y barra de menu

--- src/fw/fw.module.ts
+++ src/fw/fw.module.ts
@@ -2,10 +2,14 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { FrameworkBodyComponent } from './framework-body/framework-body.component';
+import { ContentComponent } from './content/content.component';
+import { TitleBarComponent } from './title-bar/title-bar.component';
 
 @NgModule({
   declarations: [
-    FrameworkBodyComponent
+    FrameworkBodyComponent,
+    ContentComponent,
+    TitleBarComponent
   ],
   imports: [
     CommonModule
